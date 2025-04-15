07bb90fcfa1ef0b7d15c86374295853b3b2434ba
ropehapi <ropehapi@gmail.com>
2025-04-15 00:38:13 -0300

    Adicionando failover

--- service-b/main.go
+++ service-b/main.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/hello-a", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello from service B (failover)")
+	})
 	http.HandleFunc("/hello-b", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello from Service B!")
 	})
